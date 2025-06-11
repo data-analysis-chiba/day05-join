be16d8ae86981b0680c17d7c7abaaf2b977709ef
Joel Nitta <joelnitta@gmail.com>
2025-06-11 14:37:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# day05-join
+
+Instructions for completing this assignment:
+
+Clone this repo, edit the code in [`day05_homework.R`](day05_homework.R), commit your changes, and push them to the remote.
---
+++ day05_homework.R
@@ -0,0 +1,53 @@
+# DAY 5 HOMEWORK
+#
+# Name:
+
+# INSTRUCTIONS:
+#
+# Any un-commented text you write must be valid R code. In other words, you
+# should be able to run this script without errors. Any other text (such as
+# answers to questions) should be commented. You must show the code that
+# you used to answer the question.
+#
+# I show an example below:
+#
+# Example
+#
+# 0. What is the name of the package that includes flight data from airports
+#    in New York for 2013? Load the package.
+
+# Answer: nycflights13
+
+# Code:
+
+library(nycflights13)
+
+# 1. The `nycflights13` package includes a data frame called `planes` that
+#    contains data for each airplane. Using the `planes` and `flights` data
+#    frames, identify the top two manufacturers of the airplanes that had the
+#    most flights.
+
+# Answer
+# Top two manufacturers:
+
+# Code:
+
+
+
+
+# 2. Does every departing flight have corresponding weather data for that hour?
+#    HINT: use a filtering join to check.
+
+# Answer: yes or no
+
+# Code
+
+
+
+
+# 3. What was the wind-speed at the originating airport for the flight with the
+#    biggest departure delay?
+
+# Answer:
+
+# Code
